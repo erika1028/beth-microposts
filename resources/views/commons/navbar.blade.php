183eafe6f151ada1289ce07cf5bc7ccc2028eb75
erika.tokumoto <>
2018-06-13 06:47:47 +0000

    fixed error favorite

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -23,8 +23,8 @@
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li>{!! link_to_route('users.show', 'My Profile', ['id' => $user->id]) !!} </li>
-                                <li>{!! link_to_route('users.favorites', 'Favorite', ['id' => $user->id]) !!} </a></li>
+                                <li>{!! link_to_route('users.show', 'My Profile', ['id' => Auth::user() ->id]) !!} </li>
+                                <li>{!! link_to_route('users.favorites', 'Favorite', ['id' => Auth::user()->id]) !!} </li>
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
